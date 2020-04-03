deaed6fdb77ac6723ed26c03e812195d3dff86d6
Pasat Sergiu <spasat@gmail.com>
2020-04-03 16:56:26 +0300

    Update profile

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -5,16 +5,23 @@ namespace App\Controller\Api;
 use App\Controller\AbstractApiController;
 use App\Entity\User;
 use App\Form\UserRegisterType;
+use App\Form\UserUpdateType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationExpiredException;
 
+/**
+ * @Route("/users")
+ * Class UserController
+ * @package App\Controller\Api
+ */
 class UserController extends AbstractApiController
 {
     /**
-     * @Route("/users/me", name="users.current")
+     * @Route("/me", name="users.current", methods={"GET"})
      * @return Response
      */
     public function current(): Response
@@ -23,7 +30,7 @@ class UserController extends AbstractApiController
     }
 
     /**
-     * @Route("/users/register", name="users.register", methods={"POST"})
+     * @Route("/register", name="users.register", methods={"POST"})
      * @param Request $request
      * @param EntityManagerInterface $em
      * @param UserPasswordEncoderInterface $encoder
@@ -48,6 +55,33 @@ class UserController extends AbstractApiController
             $em->flush();
 
             return new Response(null, 201);
+        }
+
+        return $this->json(
+            [
+                'code' => 400,
+                'errors' => $this->getValidationErrors($form)
+            ],
+            400
+        );
+    }
+
+    /**
+     * @Route("/profile", name="users.update", methods={"PUT"})
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function update(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserUpdateType::class, $user, ['method' => 'PUT']);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em->flush();
+            return $this->json($user, 200);
         }
 
         return $this->json(
---
+++ src/Form/UserUpdateType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Required;
+
+class UserUpdateType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class, ['constraints' => [new Required()]])
+            ->add('lastName', TextType::class, ['constraints' => [new Required()]]);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => User::class,
+                'csrf_protection' => false,
+            ]
+        );
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+}
